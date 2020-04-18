295dcc01325e9847da82e4751e1402d4d2e7ead0
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-18 14:05:03 +0200

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+extern crate simple_logger;
+
 use std::collections::HashMap;
-use rand::seq::SliceRandom;
 use std::ops::Div;
+
+use rand::seq::SliceRandom;
+
+use log::*;
 
 mod rider;
 mod locations;
@@ -13,6 +18,8 @@ const MIN_T: u32 = 1;
 const MAX_T: u32 = 100;
 
 fn main() {
+    simple_logger::init().expect("Can't initialize logging.");
+
     let range = (MIN_TIP..MAX_TIP).collect::<Vec<u32>>();
     let mut rng = rand::thread_rng();
     let mut locations = HashMap::new();
@@ -26,12 +33,12 @@ fn main() {
         .collect::<Vec<u32>>();
 
     locations_sorted.sort(); locations_sorted.reverse();
-    println!("{:?}", locations_sorted);
+    info!("{:?}", locations_sorted);
 
     let locations_values_sum = locations.values()
         .map(|x| **x)
         .sum::<u32>();
-    println!("Tot: {:?}", locations_values_sum);
+    info!("Tot: {:?}", locations_values_sum);
 
     let locations_values_sum_f = locations_values_sum as f32;
     let num_riders_f = NUM_RIDERS as f32;
@@ -39,5 +46,5 @@ fn main() {
     let opt_relax = max_opt_relax -
         (locations_values_sum_f - max_opt_relax) /
             (num_riders_f - 1.0);
-    println!("Z* Relaxed: {:?}", opt_relax);
+    info!("Z* Relaxed: {:?}", opt_relax);
 }

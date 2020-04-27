0e904ebcb5b900c05af97deaa7943f6e8e65efcc
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-27 16:43:21 +0200

    Add part of the search algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate simple_logger;
 
 use std::collections::HashMap;
-use std::ops::Div;
 
 use rand::seq::SliceRandom;
 
+use crate::rider::Rider;
 use log::*;
 
 mod rider;
@@ -13,32 +13,89 @@ const NUM_LOCATIONS: u32 = 15;
 const NUM_RIDERS: u32 = 5;
 const MIN_TIP: u32 = 1;
 const MAX_TIP: u32 = 10;
-const MIN_T: u32 = 1;
-const MAX_T: u32 = 100;
+
+struct CgaTree {
+    best_difference: f64,
+    nodes_number: u32,
+    finished: bool,
+}
+
+impl CgaTree {
+    fn new(best_difference: f64) -> Self {
+        Self {
+            best_difference,
+            nodes_number: 0,
+            finished: false,
+        }
+    }
+
+    fn search(&mut self, locations: HashMap<u32, f64>, riders: HashMap<u32, Rider>) {
+        if !self.finished {
+            self.nodes_number += 1;
+
+            let mut locations_sorted = locations.values().collect::<Vec<_>>();
+            locations_sorted.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            locations_sorted.reverse();
+
+            let mut riders_sorted = riders.values().collect::<Vec<_>>();
+            riders_sorted.sort();
+
+            let mut sum_riders = 0.0;
+            for r in riders_sorted[..riders_sorted.len() - 1].iter() {
+                sum_riders += r.sum_tips();
+            }
+
+            let locations_values_sum = locations.values().sum::<f64>();
+
+            let sum_tot = sum_riders + locations_values_sum;
+
+            let max_rider = riders_sorted.last().unwrap().sum_tips();
+
+            let val = max_rider - (sum_tot / (NUM_RIDERS - 1) as f64);
+
+            let _max_one_tree_max = 0.0;
+            let _one_tree_bound = false;
+
+            if riders_sorted.last().unwrap().orders.keys().len() >= 4 && val < self.best_difference
+            {
+                info!("First branch!");
+            }
+        }
+    }
+}
 
 fn main() {
     simple_logger::init().expect("Can't initialize logging.");
 
-    let range = (MIN_TIP..MAX_TIP).collect::<Vec<u32>>();
+    let range = (MIN_TIP..MAX_TIP).collect::<Vec<_>>();
     let mut rng = rand::thread_rng();
     let mut locations = HashMap::new();
+    let mut riders = HashMap::new();
 
     for i in 0..NUM_LOCATIONS {
-        locations.insert(i, range.choose(&mut rng).unwrap());
+        locations.insert(i, *range.choose(&mut rng).unwrap() as f64);
     }
 
-    let mut locations_sorted = locations.values().map(|x| **x).collect::<Vec<u32>>();
+    for i in 0..NUM_RIDERS {
+        let mut rider = Rider::new(i);
+        rider.add_tip(NUM_LOCATIONS, 10.0);
+        riders.insert(i, rider);
+    }
 
-    locations_sorted.sort();
+    let mut locations_sorted = locations.values().collect::<Vec<_>>();
+
+    locations_sorted.sort_by(|a, b| a.partial_cmp(b).unwrap());
     locations_sorted.reverse();
     info!("{:?}", locations_sorted);
 
-    let locations_values_sum = locations.values().map(|x| **x).sum::<u32>();
+    let locations_values_sum = locations.values().sum::<f64>();
     info!("Tot: {:?}", locations_values_sum);
 
-    let locations_values_sum_f = locations_values_sum as f32;
-    let num_riders_f = NUM_RIDERS as f32;
-    let max_opt_relax = (locations_values_sum_f / num_riders_f).ceil();
-    let opt_relax = max_opt_relax - (locations_values_sum_f - max_opt_relax) / (num_riders_f - 1.0);
+    let num_riders_f = NUM_RIDERS as f64;
+    let max_opt_relax = (locations_values_sum / num_riders_f).ceil();
+    let opt_relax = max_opt_relax - (locations_values_sum - max_opt_relax) / (num_riders_f - 1.0);
     info!("Z* Relaxed: {:?}", opt_relax);
+
+    let mut cga_tree = CgaTree::new(locations_values_sum);
+    cga_tree.search(locations, riders);
 }
--- src/rider.rs
+++ src/rider.rs
@@ -3,30 +3,39 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Rider {
-    orders: HashMap<u32, u32>,
-    route: Option<Vec<u32>>,
+    id: u32,
+    pub orders: HashMap<u32, f64>,
+    pub route: Option<Vec<u32>>,
 }
 
 impl Rider {
-    pub fn new() -> Rider {
+    pub fn new(id: u32) -> Rider {
         Rider {
+            id,
             orders: HashMap::new(),
             route: None,
         }
     }
 
-    pub fn add_tip(&mut self, location: u32, tip: u32) {
+    pub fn add_tip(&mut self, location: u32, tip: f64) {
         self.orders.insert(location, tip);
     }
 
-    pub fn sum_tips(&self) -> u32 {
+    pub fn sum_tips(&self) -> f64 {
         self.orders
             .keys()
-            .into_iter()
             .map(|&k| self.orders.get(&k).unwrap())
-            .sum::<u32>()
+            .sum::<_>()
     }
 }
+
+impl Ord for Rider {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.sum_tips().partial_cmp(&other.sum_tips()).unwrap()
+    }
+}
+
+impl Eq for Rider {}
 
 impl PartialOrd for Rider {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
@@ -52,32 +61,32 @@ mod tests {
 
     #[test]
     fn sum_tips() {
-        let mut rider = Rider::new();
-        rider.add_tip(0, 9);
-        rider.add_tip(1, 10);
+        let mut rider = Rider::new(0);
+        rider.add_tip(0, 9.0);
+        rider.add_tip(1, 10.0);
 
-        assert_eq!(rider.sum_tips(), 19);
+        assert_eq!(rider.sum_tips(), 19.0);
     }
 
     #[test]
     fn sum_tips_override_tip() {
-        let mut rider = Rider::new();
-        rider.add_tip(0, 9);
-        rider.add_tip(0, 8);
-        rider.add_tip(1, 10);
+        let mut rider = Rider::new(0);
+        rider.add_tip(0, 9.0);
+        rider.add_tip(0, 8.0);
+        rider.add_tip(1, 10.0);
 
-        assert_eq!(rider.sum_tips(), 18);
+        assert_eq!(rider.sum_tips(), 18.0);
     }
 
     #[test]
     fn partial_ord() {
-        let mut rider_1 = Rider::new();
-        rider_1.add_tip(0, 9);
-        rider_1.add_tip(1, 10);
+        let mut rider_1 = Rider::new(0);
+        rider_1.add_tip(0, 9.0);
+        rider_1.add_tip(1, 10.0);
 
-        let mut rider_2 = Rider::new();
-        rider_2.add_tip(0, 5);
-        rider_2.add_tip(1, 10);
+        let mut rider_2 = Rider::new(1);
+        rider_2.add_tip(0, 5.0);
+        rider_2.add_tip(1, 10.0);
 
         assert_eq!(rider_2 < rider_1, true);
         assert_eq!(rider_1 > rider_2, true);
